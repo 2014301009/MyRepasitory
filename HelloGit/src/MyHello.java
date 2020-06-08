8fc96b04a971b1e71e7cd23b30808748f9635e2c
Kisoo <Kisoo@DESKTOP-CUI2F6B>
2020-06-09 00:42:37 +0900

    First commit in dev branch

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -1,5 +1,7 @@
 public class MyHello {
 	public static void sayHello() {
-		System.out.println("Hello!");
+		System.out.println("Hello1");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
 	}
 }
